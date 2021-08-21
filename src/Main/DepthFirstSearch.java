60268bb6e6823a2254207df099652015aa0f06b2
Atharva Naphade <naphade21@gmail.com>
2021-08-21 21:35:36 +0530

    Updated README.md

--- src/Main/DepthFirstSearch.java
+++ src/Main/DepthFirstSearch.java
@@ -9,12 +9,28 @@ import java.util.*;
 public class DepthFirstSearch {
 	
 	class Node {
-		int val;
-		ArrayList<Node> children;
+		ArrayList<Node> adj=new ArrayList<>();
+		int dist;
 	}
 	
 	public static void main (String[] args) throws IOException {
 		;
+	}
+	
+	static void BFSIterative(Node from, Node[] nodes) {
+		for (Node nn:nodes) nn.dist=-1;
+		from.dist=0;
+		ArrayDeque<Node> bfs=new ArrayDeque<>();
+		bfs.add(from);
+		while (!bfs.isEmpty()) {
+			Node next=bfs.remove();
+			for (Node nn:next.adj) {
+				if (nn.dist==-1) {
+					nn.dist=next.dist+1;
+					bfs.add(nn);
+				}
+			}
+		}
 	}
 	
 	public static void DFSRecursive(ArrayList<Node> children) {
