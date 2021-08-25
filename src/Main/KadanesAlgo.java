24509f3e3b04033cf55320ccd172d0119190249a
Atharva Naphade <naphade21@gmail.com>
2021-08-25 19:28:15 +0530

    Added Problems

---
+++ src/Main/KadanesAlgo.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+public class KadanesAlgo {
+	public static void main (String[] args) throws IOException {
+		;
+	}
+	
+	public static void Solution () {
+		;
+	}
+}
