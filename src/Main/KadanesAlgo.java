a279d9a45cbe9e095a2e15c0b31d197858bf4a4f
Atharva Naphade <naphade21@gmail.com>
2021-08-26 17:48:31 +0530

    Added Problems

--- src/Main/KadanesAlgo.java
+++ src/Main/KadanesAlgo.java
@@ -3,12 +3,23 @@ package Main;
 import java.io.*;
 import java.util.*;
 
+// To solve maximum sum sub array.
+// maxSum = max (prevSum, currNum)
+
 public class KadanesAlgo {
 	public static void main (String[] args) throws IOException {
-		;
+		int[] arr = { 3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4 };
+		Solution(arr);
 	}
 	
-	public static void Solution () {
-		;
+	// O(N) Time
+	// O(1) Space
+	public static void Solution (int[] arr) {
+		int ret = arr[0], maxSoFar = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			maxSoFar = Math.max (arr[i], maxSoFar + arr[i]);
+			ret = Math.max(ret, maxSoFar);
+		}
+		System.out.println(ret);
 	}
 }
