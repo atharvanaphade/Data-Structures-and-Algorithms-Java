391ced4047ac58ebbee4280a4faa8addcad22875
Atharva Naphade <naphade21@gmail.com>
2021-08-31 20:03:23 +0530

    Added Problems

--- src/Main/LongestPalindromicSubstring.java
+++ src/Main/LongestPalindromicSubstring.java
@@ -7,16 +7,15 @@ import java.util.*;
 
 public class LongestPalindromicSubstring {
 	public static void main (String[] args) throws IOException {
-		String a = "sabbacc";
+		String a = "sabbasc";
 		Solution(a);
 	}
 	
 	public static void Solution (String s) {
 		int m = s.length();
 		int[][] dp = new int[m + 1][m + 1];
-		dp[0][0] = 1;
 		int ret = 0;
-		for (int i = 1; i < m; i++) {
+		for (int i = 1; i <= m; i++) {
 			for (int j = m; j > 0; j--) {
 				if (s.charAt(i - 1) == s.charAt(j - 1)) {
 					dp[i][m - j + 1] = dp[i - 1][m - j] + 1;
