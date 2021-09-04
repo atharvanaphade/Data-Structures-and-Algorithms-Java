c4b8c45e24d6b44b5f9e1dc3bd72bc388a549422
Atharva Naphade <naphade21@gmail.com>
2021-09-04 17:14:35 +0530

    Added Problems

--- src/Main/FourNumberSum.java
+++ src/Main/FourNumberSum.java
@@ -5,6 +5,6 @@ import java.util.*;
 
 // 
 
-public class FastFourierTransform {
+public class FourNumberSum {
 
 }
---
+++ src/Main/KMP.java
@@ -0,0 +1,16 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+// Check if one string contains another string. (Pattern Matching)
+
+public class KMP {
+	public static void main (String[] args) throws IOException {
+		;
+	}
+	
+	public static void Solution () {
+		
+	}
+}
