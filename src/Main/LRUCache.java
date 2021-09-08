96e2809b3361db7965f68c613812e362041683ca
Atharva Naphade <naphade21@gmail.com>
2021-09-08 14:23:22 +0530

    Added Problems

---
+++ src/Main/LRUCache.java
@@ -0,0 +1,17 @@
+package Main;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+// Implement using LL and HashMap.
+
+public class LRUCache {
+	
+}
