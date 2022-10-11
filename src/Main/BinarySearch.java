74bf2c42501fb8a5053151b64e44653983c7f2ba
UmakanthSahu <69105946+UmakanthSahu@users.noreply.github.com>
2022-10-11 19:56:02 +0530

    edge case error updated in BinarySearch.java

--- src/Main/BinarySearch.java
+++ src/Main/BinarySearch.java
@@ -1,47 +1,56 @@
 package Main;
 
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
 
 /**
  * <h1>Binary Search</h1>
  * 
- * <p>Bin Search is a searching algorithm which finds a element in a sorted array in O(logN) time.</p>
+ * <p>
+ * Bin Search is a searching algorithm which finds a element in a sorted array
+ * in O(logN) time.
+ * </p>
  * 
  * @author atharvanaphade
  *
  */
 
 public class BinarySearch {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		ArrayList<Integer> a = new ArrayList<>();
 		for (int i = 1; i < 9; i++) {
 			a.add(i);
 		}
-		int t = 11;
-		BinSearch(a, t);
+		int t = 20;
+		binarySearch(a, t);
 	}
+
 	/**
-	 * Divide and Conquer, VERY IMPORTANT to understand the difference between O(N) and O(logN) time complexity.
+	 * Divide and Conquer, VERY IMPORTANT to understand the difference between O(N)
+	 * and O(logN) time complexity.
 	 * 
-	 * @param a
+	 * @param arr
 	 * @param target
 	 */
-	public static void BinSearch (ArrayList<Integer> a, int target) {
-		int n = a.size();
-		int l = 0, r = n - 1;
-		int ret = -1;
-		while (l <= r) {
-			int mid = l + (r - l) / 2;
-			if (a.get(mid) == target) {
-				ret = mid + 1;
+	public static void binarySearch(ArrayList<Integer> arr, int target) {
+		int n = arr.size();
+		int left = 0, right = n - 1, mid = 0;
+		int index = -1;
+
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+			if (arr.get(mid) == target) {
+				index = mid;
 				break;
-			} else if (a.get(mid) < target) {
-				l = mid;
-			} else if (a.get(mid) > target) {
-				r = mid;
-			} 
+			} else if (arr.get(mid) < target) {
+				left = mid + 1;
+			} else if (arr.get(mid) > target) {
+				right = mid - 1;
+			}
 		}
-		System.out.println(ret + " ");
+
+		if (index >= 0)
+			System.out.printf("Target %d found at index %d\n", target, index);
+		else
+			System.out.printf("Target %d not present in the array.\n", target);
 	}
 }
